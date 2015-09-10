99fc92c468f88250d48202cc091d823aca011560
Aleksander Grzybowski <kelog@orange.pl>
2015-09-10 10:56:47 +0200

    Working DS1307

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 all:
-	avr-gcc -mmcu=atmega328p -Wall -Os -o target/out.elf *.c -DF_CPU=1200000
+	avr-gcc -mmcu=atmega328p -Wall -Os -o target/out.elf *.c -DF_CPU=1200000 -std=c99
 	avr-objcopy -j .text -j .data -O ihex target/out.elf target/out.hex
 	sudo avrdude  -p m328p -c usbasp -P usb -B 4 -U flash:w:target/out.hex
--- main.c
+++ main.c
@@ -7,16 +7,39 @@
 #include <avr/eeprom.h>
 
 #include "HD44780.h"
+#include "ds1307.h"
+
+typedef struct {
+	uint8_t hour;
+	uint8_t minute;
+	uint8_t second;
+} Time;
+
+void set_time(Time time) {
+	ds1307_setdate(1, 1, 1, time.hour, time.minute, time.second);
+}
+
+Time get_time() {
+	uint8_t dummy;
+	Time result;
+	ds1307_getdate(&dummy, &dummy, &dummy, &result.hour, &result.minute, &result.second);
+	return result;
+}
 
 int main() {
 	LCD_Initalize();
 
-	char buf[10];
-	int i = 0;
+	char buf[16];
+
+	Time new_time = {12, 34, 56};
+	set_time(new_time);
 
 	while (1) {
 		LCD_Clear();
-		sprintf(buf, "Hello %d", i++);
+
+		Time now = get_time();
+		sprintf(buf, "Now %02d:%02d:%02d", now.hour, now.minute, now.second);
+
 		LCD_WriteText(buf);
 		_delay_ms(1000);
 	}
